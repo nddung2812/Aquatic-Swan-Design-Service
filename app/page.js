b023c39017d1427e6f30ee11b669b8b8846b053e
unknown <nddung2812@gmail.com>
2024-04-02 22:10:26 +1000

    convert to Next JS

--- app/page.js
+++ app/page.js
@@ -1,16 +1,13 @@
-'use client'
 import WaterWave from 'react-water-wave';
 import './page.css';
 import Navbar from './components/Navbar';
 import Duckweeds from './components/Duckweeds';
 import Footer from './components/Footer';
 import HomeBanner from './components/HomeBanner';
-import { Preloader } from "./components/Preloader.jsx"
 
 const Home = () => {
   return (
     <>
-    <Preloader />
     <WaterWave
       dropRadius={60}
       perturbance={0.05}
